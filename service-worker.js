b63df257927b755c6494f3bd1c1b56730978b07a
Kevin Oyowe <kevinoyowe@gmail.com>
2019-02-21 03:04:14 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -135,6 +135,7 @@ this.addEventListener('fetch', event => {
 	addBtn.style.display = 'none';
 
 	window.addEventListener('beforeinstallprompt', (e) => {
+		debugger;
 	  // Prevent Chrome 67 and earlier from automatically showing the prompt
 	  e.preventDefault();
 	  // Stash the event so it can be triggered later.
