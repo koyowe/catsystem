9fac78579d7b151755dd699fcf2969af5e372f71
Kevin Oyowe <kevinoyowe@gmail.com>
2019-02-21 03:19:48 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -135,11 +135,11 @@ const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
-	Prevent Chrome 67 and earlier from automatically showing the prompt
+	//Prevent Chrome 67 and earlier from automatically showing the prompt
 	e.preventDefault();
-	Stash the event so it can be triggered later.
+	//Stash the event so it can be triggered later.
 	deferredPrompt = e;
-	Update UI to notify the user they can add to home screen
+	//Update UI to notify the user they can add to home screen
 	addBtn.style.display = 'block';
 
 	addBtn.addEventListener('click', (e) => {
