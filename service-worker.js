37cb8e04763437e3451ef02b9697bb0d4491abba
Kevin Oyowe <kevinoyowe@gmail.com>
2019-02-21 03:52:36 +0300

    attempting app installer

--- service-worker.js
+++ service-worker.js
@@ -82,6 +82,74 @@
   // Ensure that our service worker takes control of the page as soon as possible.
   global.addEventListener('install', event => event.waitUntil(global.skipWaiting()));
   global.addEventListener('activate', event => event.waitUntil(global.clients.claim()));
+  
+  
+  
+  (function () {
+
+	  var deferredPrompt;
+
+	  window.addEventListener('beforeinstallprompt', function (e) {
+		// Prevent Chrome 67 and earlier from automatically showing the prompt
+		e.preventDefault();
+		// Stash the event so it can be triggered later.
+		deferredPrompt = e;
+
+		showAddToHomeScreen();
+
+	  });
+
+	function showAddToHomeScreen() {
+
+	  var a2hsBtn = document.querySelector(".add-button");
+
+	  a2hsBtn.style.display = "block";
+
+	  a2hsBtn.addEventListener("click", addToHomeScreen);
+
+	}
+
+	  function addToHomeScreen() {
+
+		var a2hsBtn = document.querySelector(".add-button");
+
+		// hide our user interface that shows our A2HS button
+		a2hsBtn.style.display = 'none';
+
+		if (deferredPrompt) {
+		  // Show the prompt
+		  deferredPrompt.prompt();
+
+		  // Wait for the user to respond to the prompt
+		  deferredPrompt.userChoice
+			.then(function (choiceResult) {
+
+			  if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			  } else {
+				console.log('User dismissed the A2HS prompt');
+			  }
+
+			  deferredPrompt = null;
+
+			});
+
+		}
+
+	  }
+
+	  showAddToHomeScreen();
+
+	  window.addEventListener('appinstalled', function (evt) {
+		console.log('a2hs', 'installed');
+	  });
+
+
+	})();
+  
+  
+  
+  
 })(self);
 
 function getFilenameFromUrl(path){
